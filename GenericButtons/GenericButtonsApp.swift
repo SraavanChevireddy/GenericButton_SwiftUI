5ef47f22f5a52018a29276c8afde6e2f117a5632
Sraavan Chevireddy <schevireddy@saventech.com>
2022-07-20 02:43:39 +0530

    Initial Commit

---
+++ GenericButtons/GenericButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericButtonsApp.swift
+//  GenericButtons
+//
+//  Created by Sraavan Chevireddy on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct GenericButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
